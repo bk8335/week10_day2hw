71ddabb5706731c5f8ee1e82d2fcfb0bb15abd0e
Benjamin <benjamin.kinnard1@gmail.com>
2017-05-09 17:55:08 +0100

    can delete dinosaurs of same type properly

--- park.js
+++ park.js
@@ -2,10 +2,32 @@ var Park = function() {
   this.enclosure = [];
 }
 
-  Park.prototype = {
-    add: function(dinosaur) {
-      this.enclosure.push(dinosaur);
+Park.prototype = {
+  add: function(dinosaur) {
+    this.enclosure.push(dinosaur);
+  },
+
+  // removeByType: function(dinosaurType){
+  //   for(var dinosaur of this.enclosure){
+  //     if(dinosaur.type === dinosaurType){
+  //       var index = this.enclosure.indexOf(dinosaur);
+  //       this.enclosure.splice(index, 1);
+  //     }
+  //   }
+  // },
+
+  removeByType: function(dinosaurType){
+    for (index = this.enclosure.length - 1; index>=0; index --){
+      if(this.enclosure[index].type === dinosaurType){
+        this.enclosure.splice(index,1);
+      }
     }
-  };
+  },
+
+  returnDinosaursWithTwoKids: function(){
+
+  }
+
+};
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -9,6 +9,8 @@ describe("Park", function() {
   beforeEach(function() {
     park1 = new Park();
     dinosaur2 = new Dinosaur("Velociraptor", 8);
+    dinosaur3 = new Dinosaur("Triceratops", 1);
+    dinosaur4 = new Dinosaur("Triceratops", 12);
   })
 
   it("park enclosure starts empty", function() {
@@ -19,6 +21,23 @@ describe("Park", function() {
     park1.add(dinosaur2);
     assert.equal(1, park1.enclosure.length);
   })
+
+  it("remove all Triceratops from the park", function() {
+    park1.add(dinosaur2);
+    park1.add(dinosaur3);
+    park1.add(dinosaur4);
+    assert.equal(3, park1.enclosure.length);
+    park1.removeByType("Triceratops");
+    assert.equal(1, park1.enclosure.length);
+  })
+
+  // it("return all dinosaurs with >2 children", function() {
+  //   park1.add(dinosaur2);
+  //   park1.add(dinosaur3);
+  //   park1.add(dinosaur4);
+  //   var result = park1.returnDinosaursWithTwoKids();
+  //   assert.equal(2, result.length);
+  // })
 
 });
 
